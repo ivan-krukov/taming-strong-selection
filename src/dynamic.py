355a333f0f7dd8ae5257f17517bec794c4706320
Ivan <krukov@scabiosa.local>
2019-11-13 17:13:51 -0500

    Added matrix building functions

--- src/dynamic.py
+++ src/dynamic.py
@@ -2,10 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from functools import lru_cache
 
-N = 10
-
 @lru_cache(maxsize=500)
-def P(j, i, n, depth=0, verbose=False):
+def P(j, i, n, N, depth=0, verbose=False):
     """Neutral sample building"""
 
     # v = 0
@@ -32,10 +30,10 @@ def P(j, i, n, depth=0, verbose=False):
             raise Exception("Impossible")
 
     else:
-        j1i1 = P(j-1, i-1, n-1, depth+1, verbose)
-        j1i0 = P(j-1, i  , n-1, depth+1, verbose)
-        j0i1 = P(j  , i-1, n-1, depth+1, verbose)
-        j0i0 = P(j  , i  , n-1, depth+1, verbose)
+        j1i1 = P(j-1, i-1, n-1, N, depth+1, verbose)
+        j1i0 = P(j-1, i  , n-1, N, depth+1, verbose)
+        j0i1 = P(j  , i-1, n-1, N, depth+1, verbose)
+        j0i0 = P(j  , i  , n-1, N, depth+1, verbose)
 
         # derived picked
         d  = j1i1 * (1 - ((n-1) / N))
@@ -54,9 +52,8 @@ def P(j, i, n, depth=0, verbose=False):
 
     return v
 
-matrices = [np.full((i + 1, i + 1), np.nan) for i in range(50 + 1)]
 
-def Pd(j, i, n):
+def Pd(j, i, n, N, matrices):
     
     if (n == 0) or (i < 0) or (j < 0) or (j > n) or (i > n):
         v = 0
@@ -75,10 +72,10 @@ def Pd(j, i, n):
     else:
         v = matrices[n][j, i]
         if np.isnan(v):
-            j1i1 = Pd(j-1, i-1, n-1)
-            j1i0 = Pd(j-1, i  , n-1)
-            j0i1 = Pd(j  , i-1, n-1)
-            j0i0 = Pd(j  , i  , n-1)
+            j1i1 = Pd(j-1, i-1, n-1, N, matrices)
+            j1i0 = Pd(j-1, i  , n-1, N, matrices)
+            j0i1 = Pd(j  , i-1, n-1, N, matrices)
+            j0i0 = Pd(j  , i  , n-1, N, matrices)
 
             # derived picked
             d  = j1i1 * (1 - ((n-1) / N))
@@ -94,3 +91,13 @@ def Pd(j, i, n):
             matrices[n][j, i] = v
 
     return v
+
+
+def build_mtx(n, N):
+    matrices = [np.full((i + 1, i + 1), np.nan) for i in range(n + 1)]
+
+    Q = np.zeros((n + 1, n + 1))
+    for i in range(0, n + 1):
+        for j in range(0, n + 1):
+            Q[j, i] = Pd(j, i, n, N, matrices)
+    return Q
