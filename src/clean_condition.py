b5c757be196b3f0a947a49adb5c541edeef822e7
Ivan Krukov <ivan.o.kryukov@gmail.com>
2019-12-18 16:14:58 -0500

    wip - number of contributing lineages

--- src/clean_condition.py
+++ src/clean_condition.py
@@ -36,11 +36,13 @@ def Qn(io, no, ic, nc, N, cache, n):
     return v
 
 
-def Qs(io, no, ic, nc, N, s, cache):
+def Qs(io, no, ic, nc, N, s, cache, dense_cache = False):
     if (no < 1) or (nc < 1) or (io < 0) or (ic < 0) or (io > no) or (ic > nc):
         return 0
-    v = cache[nc-1][no-1][ic, io]
-    # v = cache[nc-1,no-1,ic,io]
+    if dense_cache:
+        v = cache[nc,no,ic,io]
+    else:
+        v = cache[nc-1][no-1][ic, io]
     if np.isnan(v):
         if (io, no, ic, nc) == (1, 1, 1, 1):
             v = 1 - s
@@ -88,9 +90,11 @@ def Qs(io, no, ic, nc, N, s, cache):
             Q6d = 0 if Q == 0 else (1-((nc-1)/N)) * ic/nc * s * (ic-1) / N * Q
             
             v = Q1a + Q1d + Q2a + Q2d + Q3a + Q3d + Q4a + Q4d + Q5a + Q5d + Q6a + Q6d
-        
-        cache[nc-1][no-1][ic, io] = v
-        # cache[nc-1,no-1,ic,io] = v
+
+        if dense_cache:
+            cache[nc,no,ic,io] = v
+        else:
+            cache[nc-1][no-1][ic, io] = v
  
     return v
 
@@ -125,7 +129,7 @@ def matrix_selection(n, N, s=0):
     mtx = np.zeros((n + 1, n + 1))
     # cache = np.full((n+1, n+1, n+1, n+1), np.nan)
     cache = [[np.full((i+1, j+1), np.nan) for j in range(1, n+1)] for i in range(1, n+1)]
-    for nc in range(0, n + 1):  # This can be (1, n+1)
+    for nc in range(0, n + 1):
         for ic in range(0, nc + 1):
             # vectorized over ip
             p = hypergeom.pmf(ic, n, np.arange(0, n+1), nc)
@@ -134,31 +138,50 @@ def matrix_selection(n, N, s=0):
     return mtx, cache
 
 
-def plot_cache(cache, log=True):
-    n = len(cache)
-    fig, ax = plt.subplots(nrows=n, ncols=n)
-    for i in range(n):
-        for j in range(n):
-            ax[i,j].set(xlim=(-0.5,n+0.5), ylim=(-0.5,n+0.5))
+def plot_cache(cache):
+    n0 = len(cache)
+    n1 = len(cache[0]) 
+    fig, ax = plt.subplots(nrows=n0, ncols=n1)
+    for i in range(n0):
+        for j in range(n1):
+            ax[i,j].set(xlim=(-0.5,n0+0.5), ylim=(-0.5,n1+0.5))
             ax[i,j].set_axis_off()
             ax[i,j].matshow(cache[i][j])
+    return fig
+
+def plot_dense_cache(cache):
+    n0 = len(cache)
+    n1 = len(cache[0]) 
+    fig, ax = plt.subplots(nrows=n0, ncols=n1)
+    for i in range(n0):
+        for j in range(n1):
+            ax[i,j].set(xlim=(-0.5,n0+0.5), ylim=(-0.5,n1+0.5))
+            ax[i,j].set_axis_off()
+            ax[i,j].matshow(cache[i,j])
     return fig
 
 
 # WIP
 def matrix_double_sample_selection(n, N, s=0):
-    mtx = np.zeros(((2*n) + 1, n + 1))
-    cache = np.full((2*n+1, 2*n+1, 2*n+1, 2*n+1), np.nan)
-    for nc in range(0, (2*n) + 1):  # This can be (1, n+1)
-        for ic in range(0, nc + 1):
-            # vectorized over ip
-            p = hypergeom.pmf(ic, 2*n, np.arange(0, (2*n)+1), nc)
-            for io in range(0, n + 1):
-                mtx[:, io] += Qs(io, n, ic, nc, N, s, cache) * p
+    mtx = np.zeros((2*n + 1, n + 1))
+    # cache = [[np.full((i+1, j+1), np.nan) for j in range(1, n+1)] for i in range(1, 2*n+1)]
+    cache =  np.full((2*n+1, 2*n+1, 2*n+1, 2*n+1), np.nan)
+    for ip in range(0, 2*n + 1):
+        for io in range(0, n + 1):
+            for nc in range(0, 2*n+1):
+                for ic in range(0, nc+1):
+                    mtx[ip, io] += Qs(io, n, ic, nc, N, s, cache, True) * hypergeom.pmf(ic, 2*n, ip, nc)
+            
+    # for nc in range(0, 2*n + 1):
+    #     for ic in range(0, nc + 1):
+    #         for io in range(0, n + 1):
+    #             for no in range(0, n):
+    #                 p = hypergeom.pmf(ic, 2*n, no, nc)
+    #                 mtx[ic, io] += Qs(io, n, ic, nc, N, s, cache, dense_cache = True) * p
     return mtx, cache
 
 
-
+# np.sum(m[:,0] * hypergeom.pmf(np.arange(0,21), 20, 0, 10))
 
 # Q_new, cache = matrix_tangle(5, 100)
 # Q_old = matrix_dumb(5, 100)
