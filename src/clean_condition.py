Correct transition probability calculation

src/clean_condition.py
@@ -57,7 +57,7 @@ def Qs(io, no, ic, nc, N, s, cache, dense_cache = False):
             Q1a = 0 if Q == 0 else (1 - ((nc-1)/N)) * (nc-ic)/nc * Q
 
             Q = Qs(io-1, no-1, ic-1, nc-1, N, s, cache)
-            Q1d = 0 if Q == 0 else (1 - ((nc-1)/N)) * ic/nc * (1-s) * Q
+            Q1d = 0 if Q == 0 else (1 - ((nc-1)/N)) * (ic)/nc * (1-s) * Q
 
             Q = Qs(io, no-1, ic, nc, N, s, cache)
             Q2a = 0 if Q == 0 else (nc-ic)/N * Q
@@ -66,22 +66,22 @@ def Qs(io, no, ic, nc, N, s, cache, dense_cache = False):
             Q2d = 0 if Q == 0 else (ic/N) * (1-s) * Q
 
             Q = Qs(io, no-1, ic-1, nc-2, N, s, cache)
-            Q3a = 0 if Q == 0 else (1 - ((nc-2)/N)) * (ic/nc) * s * (1 - ((nc-1)/N)) * Q
+            Q3a = 0 if Q == 0 else (1 - ((nc-2)/N)) * (ic/nc) * s * (1 - ((nc-1)/N)) * ((nc-ic)/(nc-1)) * Q
 
             Q = Qs(io-1, no-1, ic-2, nc-2, N, s, cache)
-            Q3d = 0 if Q == 0 else (1 - ((nc-2)/N)) * (ic/nc) * s * ((ic-1)/(nc-1)) * Q
+            Q3d = 0 if Q == 0 else (1 - ((nc-2)/N)) * (ic/nc) * s * (1 - ((nc-1)/N)) * ((ic-1)/(nc-1)) * Q
             
             Q = Qs(io, no-1, ic, nc, N, s, cache)
             Q4a = 0 if Q == 0 else (ic/N) * s * ((nc-ic)/N) * Q
             
             Q = Qs(io-1, no-1, ic, nc, N, s, cache)
-            Q4d = 0 if Q == 0 else (ic/N) * s * ((ic-1)/N) * Q
+            Q4d = 0 if Q == 0 else (ic/N) * s * ((ic)/N) * Q # this was ic-1
             
             Q = Qs(io, no-1, ic, nc-1, N, s, cache)
             Q5a = 0 if Q == 0 else (ic/N) * s * (1-((nc-1)/N)) * (nc-ic)/nc * Q
             
             Q = Qs(io-1, no-1, ic-1, nc-1, N, s, cache)
-            Q5d = 0 if Q == 0 else (ic/N) * s * (1-((nc-1)/N)) * ic/nc * Q
+            Q5d = 0 if Q == 0 else (ic/N) * s * (1-((nc-1)/N)) * (ic)/nc * Q
             
             Q = Qs(io, no-1, ic-1, nc-1, N, s, cache)
             Q6a = 0 if Q == 0 else (1-((nc-1)/N)) * ic/nc * s * (nc - ic) / N * Q
@@ -170,15 +170,7 @@ def matrix_double_sample_selection(n, N, s=0):
         for io in range(0, n + 1):
             for nc in range(0, 2*n+1):
                 for ic in range(0, nc+1):
-                    mtx[ip, io] += Qs(io, n, ic, nc, N, s, cache, True) * hypergeom.pmf(ic, 2*n, ip, nc)
-            
-    # for nc in range(0, 2*n + 1):
-    #     for ic in range(0, nc + 1):
-    #         for io in range(0, n + 1):
-    #             for no in range(0, n):
-    #                 p = hypergeom.pmf(ic, 2*n, no, nc)
-    #                 mtx[ic, io] += Qs(io, n, ic, nc, N, s, cache, dense_cache = True) * p
-    return mtx, cache
+                    mtx[ip, io] += Qs(io, n, ic, nc, N, s, cache, dense_cache=True) * hypergeom.pmf(ic, 2*n, ip, nc)
 
 
 # np.sum(m[:,0] * hypergeom.pmf(np.arange(0,21), 20, 0, 10))