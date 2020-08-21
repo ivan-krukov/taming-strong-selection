fe797c8a275bb83316d1918b8aa62e8ccc0b877d
ivan-krukov <ivan.o.kryukov@gmail.com>
2020-08-21 14:01:47 -0400

    caching for dynamic failures

--- src/comparison.py
+++ src/comparison.py
@@ -1,22 +1,27 @@
 from transition_probability_dynamic_failures import matrix,P0,Pf
 from transition_probability_selection import matrix_selection
 import numpy as np
+from time import perf_counter
 
 np.set_printoptions(precision=3, linewidth=100)
 
 Ne = 1e3
 s = 1e-2
-n = 5
+n = 100
 max_t=1
 
 print("Computing the old verion...")
+t_start = perf_counter()
 M_old, _ = matrix_selection(
     n, Ne, s
 )  # The second return is a cache of pre-built matrices
+print("Took ", perf_counter() - t_start)
 
 print("Computing the new version...")
 # This will be slow with large n
+t_start = perf_counter()
 M_new = matrix(n, s=s, N=Ne, max_t=max_t)
+print("Took ", perf_counter() - t_start)
 
 print("absolute differences")
 print(M_new - M_old)
@@ -31,18 +36,21 @@ print(M_old)
 print("new")
 print(M_new)
 
-print(P0(1, 2, 1, 2, s, Ne, max_t))
+# print(P0(1, 2, 1, 2, s, Ne, max_t))
 
-print("Pf(1, 1, 1, 1, s, Ne, max_t)")
-print(Pf(1, 1, 1, 1, s, Ne, 1, 1))
+# print("Pf(1, 1, 1, 1, s, Ne, max_t)")
+# print(Pf(1, 1, 1, 1, s, Ne, 1, 1))
 
 
-print("Pf(1, 1, 1, 1, s, Ne, 0)")
-print(Pf(1, 1, 1, 1, s, Ne, 0, 0))
+# print("Pf(1, 1, 1, 1, s, Ne, 0)")
+# print(Pf(1, 1, 1, 1, s, Ne, 0, 0))
 
-print("P0(1, 1, 1, 1, s, Ne, 0)")
-print(P0(1, 1, 1, 1, s, Ne, 0, 0))
+# print("P0(1, 1, 1, 1, s, Ne, 0)")
+# print(P0(1, 1, 1, 1, s, Ne, 0, 0))
 
 print("Expectedd order of difference: ", s/Ne)
 
-print("Max relative difference: ", np.max(np.abs(relative_diff, where=~np.isnan(relative_diff))))
+material_differences = relative_diff[~np.isnan(relative_diff)]
+
+print("Max relative difference: ", np.max(np.abs(material_differences)))
+
--- src/transition_probability_dynamic_failures.py
+++ src/transition_probability_dynamic_failures.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import hypergeom, binom
+from numba import jit
 
+@jit(nopython=True)
 def basecases_t(io, no, ic, nc, s, t):
     if (
         (no < 0)
@@ -22,71 +24,84 @@ def basecases_t(io, no, ic, nc, s, t):
     else:
         return -1
 
-
-def Pf(io, no, ic, nc, s, N, t, max_t):
+@jit(nopython=True)
+def Pf(io, no, ic, nc, s, N, t, max_t, cache):
     """The probability of not having had a transition after t failures"""
 
     v = basecases_t(io, no, ic, nc, s, t)
+
     #print("v in pf", v, "for params ",io, no, ic, nc, s, t)
     if v == -1:
         # t is the number of failures
         if t <= 0:
             # We will build a recursion over the number of successfully drawn offspring n_0.
             # We need to start our recursion on
-            v = P0(io, no, ic, nc, s, N, max_t=max_t)
+            v = P0(io, no, ic, nc, s, N, max_t, cache)
 
         else:  # the curent number of failures was obtained from a previous number of failures
             oos = 1 - ((nc - 1) / N)  # out-of-sample
             # B and C correspond to the cases in P0
-            b = oos * (ic / nc) * s * Pf(io, no, ic - 1, nc - 1, s, N, t - 1, max_t)
+            b = oos * (ic / nc) * s * Pf(io, no, ic - 1, nc - 1, s, N, t - 1, max_t, cache)
 
-            c = (ic / N) * s * Pf(io, no, ic, nc, s, N, t - 1, max_t)
+            c = (ic / N) * s * Pf(io, no, ic, nc, s, N, t - 1, max_t, cache)
 
             v = b + c
     return v
 
-
-def P0(io, no, ic, nc, s, N, max_t, force_success=True):
+@jit(nopython=True)
+def P0(io, no, ic, nc, s, N, max_t, cache):
     """max_t is the maximum number of failures. """
 
     v = basecases_t(io, no, ic, nc, s, t=max_t)
     if v == -1:  # Recursion on what happened in the last lineage.
-        oos = 1 - ((nc - 1) / N)  # out-of-sample
-        sel = 1 - s
+        v = cache[io, no, ic, nc]
+        if np.isnan(v):
+            oos = 1 - ((nc - 1) / N)  # out-of-sample
+            sel = 1 - s
 
-        # out of sample, ancestral
-        af = sum(Pf(io, no - 1, ic, nc - 1, s, N, t, max_t) for t in range(max_t + 1))
-        a = oos * ((nc - ic) / nc) * af
+            # out of sample, ancestral
+            af = 0
+            for t in range(max_t + 1):
+                af += Pf(io, no - 1, ic, nc - 1, s, N, t, max_t, cache)
+            a = oos * ((nc - ic) / nc) * af
 
-        # out of sample, derived
-        bf = sum(Pf(io - 1, no - 1, ic - 1, nc - 1, s, N, t, max_t) for t in range(max_t + 1))
-        b = oos * (ic / nc) * sel * bf
+            # out of sample, derived
+            bf = 0
+            for t in range(max_t+1):
+                bf += Pf(io - 1, no - 1, ic - 1, nc - 1, s, N, t, max_t, cache)
+            b = oos * (ic / nc) * sel * bf
 
-        # in sample, derived
-        cf = sum(Pf(io - 1, no - 1, ic, nc, s, N, t, max_t) for t in range(max_t + 1))
-        c = (ic / N) * sel * cf
+            # in sample, derived
+            cf = 0
+            for t in range(max_t+1):
+                cf += Pf(io - 1, no - 1, ic, nc, s, N, t, max_t, cache)
+            c = (ic / N) * sel * cf
 
-        # in sample, ancestral
-        df = sum(Pf(io, no - 1, ic, nc, s, N, t, max_t) for t in range(max_t + 1))
-        d = ((nc - ic) / N) * df
-        # print("io, no, ic, nc", io, no, ic, nc)
-        # print("a,b,c, d", a,b,c,d)
-        v = a + b + c + d
-        if force_success:
-            v += (oos * (ic / nc) * s * Pf(io - 1, no - 1, ic - 1, nc - 1, s, N, max_t, max_t)) + (
-                (ic / N) * s * Pf(io - 1, no - 1, ic, nc, s, N, max_t, max_t)
+            # in sample, ancestral
+            df = 0
+            for t in range(max_t+1):
+                df += Pf(io, no - 1, ic, nc, s, N, t, max_t, cache)
+            d = ((nc - ic) / N) * df
+            # print("io, no, ic, nc", io, no, ic, nc)
+            # print("a,b,c, d", a,b,c,d)
+            v = a + b + c + d
+            v += (oos * (ic / nc) * s * Pf(io - 1, no - 1, ic - 1, nc - 1, s, N, max_t, max_t, cache)) + (
+                (ic / N) * s * Pf(io - 1, no - 1, ic, nc, s, N, max_t, max_t, cache)
             )  # Forcing success of cases b and c, respectively
+
+            cache[io, no, ic, nc] = v
     return v
 
 
 def matrix(no=5, s=1 / 1000, N=1000, max_t=1):
     mtx = np.zeros((no + 1, no + 1))
+    cache = np.full((no+1, no+1, no+1, no+1), np.nan) # this is messier (we allocate way more than we need, but it's easier for numba)
     for nc in range(0, no + 1):
         for ic in range(0, nc + 1):
             # vectorized over ip
             p = hypergeom.pmf(ic, no, np.arange(0, no + 1), nc)
             for io in range(0, no + 1):
-                mtx[:, io] += P0(io, no, ic, nc, s, N, max_t) * p
+                mtx[:, io] += P0(io, no, ic, nc, s, N, max_t, cache) * p
     return mtx
 
 def matrix_double_sample(n, N, s=0, max_t=1):
