4de55be1163a6d246f07616f243348819012ad5e
ivan-krukov <ivan.o.kryukov@gmail.com>
2020-09-16 16:50:20 -0400

    Re-ran the 'missing.pdf' figure with new dynamic matrices
    Also, this is with a smaller population size N=1000
    In the previous versions of the figure, it was N=2000, so the decrease in missing lineage was more
    dramatic. Using N=1000 for consistency with other figures

--- src/generate_tables.py
+++ src/generate_tables.py
@@ -6,16 +6,17 @@ from tqdm import tqdm
 N_range = [1000, 100, 500]
 Ns_range = [0, 10, 50]
 max_t = 4
-n = 100
+n_range = [100]
 tmp_store = Path("data")
 
-for N in tqdm(N_range):
-    for Ns in tqdm(Ns_range):
-       s = Ns / N
+for n in tqdm(n_range):
+    for N in tqdm(N_range):
+        for Ns in tqdm(Ns_range):
+           s = Ns / N
 
-       mtx_pkl = tmp_store / Path(f"mtx_n_{n}_Ns_{Ns}_N_{N}.pypkl")
-       if not mtx_pkl.exists():
-           R, _, _ = matrix(n, N=N, s=s, max_t=max_t)
-           
-           with open(mtx_pkl, "wb") as pkl:
-               pickle.dump(R, pkl)
+           mtx_pkl = tmp_store / Path(f"mtx_n_{n}_Ns_{Ns}_N_{N}.pypkl")
+           if not mtx_pkl.exists():
+               R, _, _ = matrix(n, N=N, s=s, max_t=max_t)
+
+               with open(mtx_pkl, "wb") as pkl:
+                   pickle.dump(R, pkl)
--- src/plot_closures.py
+++ src/plot_closures.py
@@ -5,26 +5,23 @@ import seaborn as sns
 from indemmar import plot_and_legend
 from pathlib import Path
 
-data_store = Path.cwd() / Path("../data")
-fig_store = Path.cwd() / Path("../fig")
+data_store = Path.cwd() / Path("data")
+fig_store = Path.cwd() / Path("fig")
 
 matrix_sets = []
-for ns in ["1.0", "5.0", "10.0"]:
-    with open(data_store / Path(f"mtxs_{ns}.pypkl"), "rb") as pkl:
-        matrix_sets.append(pickle.load(pkl))
+Ns_range = [1, 10, 50]
+n_range = np.arange(10, 140+5, 5)
+N = 1000
 
 sns.set_style("whitegrid")
 sns.set_context("paper", font_scale=1.5)
 
 with plot_and_legend(fname=fig_store / Path('missing.pdf'), legend_title="Ns") as (fig, ax):
-    for ns, mtxs in zip([1,5,10], matrix_sets):
-        closures = []
-        for m in mtxs:
-            closures.append(1-m[-1,:].sum())
-            
-        ax.semilogy(np.arange(10, 210, 10), closures, linestyle="", marker="o", label=ns)
-
-
-    ax.set(xlabel="Sample size", ylabel="Probability of missing lineages", title="N=1000")
-
+    for Ns in Ns_range:
+        missing_p = []
+        for n in n_range:
+            with open(data_store / Path(f"mtx_n_{n}_Ns_{Ns}_N_{N}.pypkl"), "rb") as pkl:
+                M = pickle.load(pkl)
+                missing_p.append(1 - (M[-1,:].sum()))
+        ax.semilogy(n_range, missing_p, label=Ns, ls="", marker="o")
         
