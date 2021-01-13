35f24bfbc173d16951d08819b2357aba10e3fb7c
ivan-krukov <ivan.o.kryukov@gmail.com>
2021-01-13 12:58:55 -0700

    c99 compat

--- src/c/transition_probability.c
+++ src/c/transition_probability.c
@@ -24,17 +24,19 @@ void matrix_set(Matrix* self, unsigned int i, unsigned int j, double v) {
 }
 
 void matrix_fill(Matrix* self, double v) {
-    for (unsigned int i = 0; i < self->rows; i++) {
-        for (unsigned int j = 0; j < self->cols; j++) {
+    unsigned int i, j;
+    for (i = 0; i < self->rows; i++) {
+        for (j = 0; j < self->cols; j++) {
             XMEM(self, i, j) = v;
         }
     }
 } 
 
 void matrix_print(Matrix* self) {
+    unsigned int i, j;
     printf("[");
-    for (unsigned int i = 0; i < self->rows; i++) {
-        for (unsigned int j = 0; j < self->cols; j++) {
+    for (i = 0; i < self->rows; i++) {
+        for (j = 0; j < self->cols; j++) {
             printf("%.3e ", XMEM(self, i, j));
         }
         if (i == self->rows - 1) {
@@ -58,8 +60,9 @@ Tile* tile_new(unsigned int rows, unsigned int cols) {
     self->rows = rows;
     self->cols = cols;
 
-    for (unsigned int i = 0; i < self->rows; i++) {
-        for (unsigned int j = 0; j < self->cols; j++) {
+    unsigned int i, j;
+    for (i = 0; i < self->rows; i++) {
+        for (j = 0; j < self->cols; j++) {
             Matrix* M = matrix_new(i+1, j+1);
             matrix_fill(M, NAN);
             XMEM(self, i, j) = M;
@@ -70,8 +73,9 @@ Tile* tile_new(unsigned int rows, unsigned int cols) {
 }
 
 void tile_print(Tile* self) {
-    for (unsigned int i = 0; i < self->rows; i++) {
-        for (unsigned int j = 0; j < self->cols; j++) {
+    unsigned int i, j;
+    for (i = 0; i < self->rows; i++) {
+        for (j = 0; j < self->cols; j++) {
             matrix_print(XMEM(self, i, j));
         }
     }
@@ -79,8 +83,9 @@ void tile_print(Tile* self) {
 
 void tile_del(Tile* self) {
 
-    for (unsigned int i = 0; i < self->rows; i++) {
-        for (unsigned int j = 0; j < self->cols; j++) {
+    unsigned int i, j;
+    for (i = 0; i < self->rows; i++) {
+        for (j = 0; j < self->cols; j++) {
             free(XMEM(self, i, j));
         }
     }
@@ -177,46 +182,3 @@ double P0(int io, int no, int ic, int nc, double s, int N, int max_t, Tile** cac
     return v;
 }
 
-/* int main() { */
-/*     /1* Sanity checks *1/ */
-/*     /1* Matrix* M = matrix_new(4, 3); *1/ */
-
-/*     /1* for (unsigned int i = 0; i < M->rows; i++) { *1/ */
-/*     /1*     for (unsigned int j = 0; j < M->cols; j++) { *1/ */
-/*     /1*         XMEM(M, i, j) = (i + 1) * (j + 1); *1/ */
-/*     /1*     } *1/ */
-/*     /1* } *1/ */
-/*     /1* matrix_print(M); *1/ */
-/*     /1* assert(XMEM(M, 0, 0) == 1); *1/ */
-
-/*     /1* matrix_fill(M, 3); *1/ */
-/*     /1* matrix_print(M); *1/ */
-/*     /1* matrix_del(M); *1/ */
-
-/*     /1* Tile* T = tile_new(3, 3); *1/ */
-/*     /1* tile_print(T); *1/ */
-/*     /1* tile_del(T); *1/ */
-
-
-/*     int N = 10000; */
-/*     double s = 1 / N; */
-/*     int no = 100; */
-/*     int max_t = 3; */
-/*     Tile** cache = malloc((max_t+1) * sizeof(Tile*)); */
-/*     for (int i = 0; i < max_t+1; i++) { */
-/*         cache[i] = tile_new(no+1, no+1); */
-/*     } */
-
-/*     for (int nc = 0; nc < no+1; nc++) { */
-/*         for (int ic = 0; ic < nc+1; ic++) { */
-/*             for (int io = 0; io < no+1; io++) { */
-/*                 P0(io, no, ic, nc,s, N, max_t, cache); */
-/*             } */
-/*         } */
-/*     } */
-
-/*     /1* tile_print(cache[0]); *1/ */
-/*     for (int i = 0; i < max_t+1; i++) { */
-/*         tile_del(cache[i]); */
-/*     } */
-/* } */
