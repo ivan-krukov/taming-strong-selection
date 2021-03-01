91c78c2ed839888d9cfeac8e865befe57665929b
ivan-krukov <ivan.o.kryukov@gmail.com>
2021-03-01 13:51:45 -0700

    added sections on fixation and load

--- src/fixation_rate_plot.py
+++ src/fixation_rate_plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import sys
 
 df = pd.read_csv(sys.argv[1])
@@ -10,29 +11,38 @@ out_file = sys.argv[3]
 genome_size = 3e9
 mutation_rate = 1e-8
 
-with plt.style.context("seaborn-whitegrid"):
-    fig, ax = plt.subplots(ncols=3, figsize=(15, 6))
+sns.set_style("whitegrid")
+sns.set_context("paper", font_scale=1.8)
 
-    df["diffusion_load"] = df.diffusion * df.dfe * N * genome_size * mutation_rate * (-df.selection)
-    df["wright_fisher_load"] = df.wright_fisher * df.dfe * N * genome_size * mutation_rate * (-df.selection)
+fig, ax = plt.subplots(ncols=3, figsize=(15, 6))
 
-    ax[0].semilogy(df.selection, df.diffusion, label="Diffusion")
-    ax[0].semilogy(df.selection, df.wright_fisher, label="Wright-Fisher")
-    ax[0].set_title("Probability of fixation of a single variant in the genome")
-    ax[0].legend()
+df["diffusion_load"] = (
+    df.diffusion * df.dfe * N * genome_size * mutation_rate * (-df.selection)
+)
+df["wright_fisher_load"] = (
+    df.wright_fisher * df.dfe * N * genome_size * mutation_rate * (-df.selection)
+)
 
-    ax[1].semilogy(df.selection, df.diffusion_load, label="Diffusion")
-    ax[1].semilogy(df.selection, df.wright_fisher_load, label="Wright-Fisher")
-    # ax[1].axhline(1 / N, color="red", ls="--")
-    ax[1].set_title("Fixation load")
-    ax[1].legend()
+ax[0].semilogy(df.selection, df.diffusion, label="Diffusion")
+ax[0].semilogy(df.selection, df.wright_fisher, label="Wright-Fisher")
+ax[0].set_title("Probability of fixation \n of a single variant in the genome")
+ax[0].text(-0.1, 1.1, "A", fontweight="bold", transform=ax[0].transAxes)
+ax[0].legend()
 
-    ax[2].semilogy(df.selection, (df.diffusion - df.wright_fisher) / df.wright_fisher)
-    # ax[2].axhline(1 / N, color="red", ls="--")
-    ax[2].set_title("Relative error of fixation probabilities")
+ax[1].semilogy(df.selection, df.diffusion_load, label="Diffusion")
+ax[1].semilogy(df.selection, df.wright_fisher_load, label="Wright-Fisher")
+# ax[1].axhline(1 / N, color="red", ls="--")
+ax[1].set_title("Fixation load")
+ax[1].text(-0.1, 1.1, "B", fontweight="bold", transform=ax[1].transAxes)
+# ax[1].legend()
 
-    fig.suptitle(
-        f"Rate of fixation in diffusion vs. Wright-Fisher, N={N}. "
-        # + "Red line - 1 fixation per genome per N generations"
-    )
-    fig.savefig(out_file, dpi=300)
+ax[2].semilogy(df.selection, (df.diffusion - df.wright_fisher) / df.wright_fisher)
+# ax[2].axhline(1 / N, color="red", ls="--")
+ax[2].set_title("Relative error of fixation probabilities")
+ax[2].text(-0.1, 1.1, "C", fontweight="bold", transform=ax[2].transAxes)
+
+# fig.suptitle(
+#     f"Rate of fixation in diffusion vs. Wright-Fisher, N={N}. "
+#     # + "Red line - 1 fixation per genome per N generations"
+# )
+fig.savefig(out_file, dpi=300)
