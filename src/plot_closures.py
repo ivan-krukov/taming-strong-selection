1013f38e7e4903c95e69ad5dbf34e19b7a968f55
ivan-krukov <ivan.o.kryukov@gmail.com>
2021-03-16 12:54:45 -0600

    jackknife figure for supplement

--- src/plot_closures.py
+++ src/plot_closures.py
@@ -11,18 +11,21 @@ fig_store = Path.cwd() / Path("fig")
 matrix_sets = []
 Ns_range = [1, 10, 50]
 n_range = np.arange(10, 140+5, 5)
+k_range = [0, 1, 2, 3]
 N = 1000
 
 sns.set_style("whitegrid")
 sns.set_context("paper", font_scale=1.5)
 
-with plot_and_legend(fname=fig_store / Path('missing.pdf'), legend_title="Ns") as (fig, ax):
+with plot_and_legend(fname=fig_store / Path('missing.pdf'), legend_title="Ns", ncol=len(k_range),
+        figsize=(12, 6)) as (fig, ax):
     for Ns in Ns_range:
-        missing_p = []
-        for n in n_range:
-            with open(data_store / Path(f"mtx_n_{n}_Ns_{Ns}_N_{N}.pypkl"), "rb") as pkl:
-                M = pickle.load(pkl)
+        for k in k_range:
+            missing_p = []
+            for n in n_range:
+                M = np.loadtxt(data_store / Path(f"q_mat_{N}_{Ns}_{n}_3_{k}.txt"))
                 missing_p.append(1 - (M[-1,:].sum()))
-        ax.semilogy(n_range, missing_p, label=Ns, ls="", marker="o")
-        ax.set(xlabel="Number of offspring, $n_o$", ylabel="Probability")
-        
+            ax[k].semilogy(n_range, missing_p, label=f"{Ns}", ls="", marker="o")
+            ax[k].set(title=f"Jackknife order {k}")
+            ax[k].set(xlabel="Number of offspring, $n_o$")
+    ax[0].set(ylabel="Probability")
