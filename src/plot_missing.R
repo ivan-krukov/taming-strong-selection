6afb11b88e6571b2a0d4b3c03b6b2c94b2c2eed5
ivan-krukov <ivan.o.kryukov@gmail.com>
2021-03-24 20:42:19 +0100

    fixed the missing probability plot

---
+++ src/plot_missing.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+missing <- read_table('data/missing_probability.tsv') %>%
+  mutate(above_critical = no > 2 * Ns) %>%
+  mutate(jackknife = as.factor(j),
+         Ns = as.factor(Ns))
+
+ggplot(missing) + geom_point(aes(x=no, y=prob, color=Ns, alpha = above_critical)) + 
+  facet_grid(~ jackknife, labeller = label_both) +
+  theme_minimal() +
+  scale_color_brewer(palette = 'Set1')  +
+  scale_y_log10() +
+  guides(alpha = F) +
+  labs(x = 'Sample size', y = 'Probability of missing a lineage') + 
+  ggsave('fig/missing.pdf')
+
